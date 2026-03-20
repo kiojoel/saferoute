301da8f055e33721b409c62c2cf84bb00bf3f2e9
kiojoel <akinsanyajoel82@gmail.com>
2026-03-20 15:38:19 +0100

    ingest incidents data to db

--- backend/pipelines/ingest_incidents.py
+++ backend/pipelines/ingest_incidents.py
@@ -4,9 +4,11 @@ import os
 
 def ingest_incidents():
     db_path = "data/processed/saferoute.sqlite"
-    csv_path = "data/raw/nigeria_incidents.csv"
+    csv_path = "data/processed/incidents_cleaned.csv"
 
     df = pd.read_csv(csv_path)
+    df['event_date'] = pd.to_datetime(df['event_date'])
+
 
     gdf_incidents = gpd.GeoDataFrame(
         df, 
@@ -17,21 +19,15 @@ def ingest_incidents():
     lga_boundaries = gpd.read_file(db_path, layer="lga_boundaries")
     
     joined_gdf = gpd.sjoin(gdf_incidents, lga_boundaries, how="left", predicate="intersects")
-
-    joined_gdf['event_date'] = pd.to_datetime(joined_gdf['date_start'])
-
+    
     final_gdf = joined_gdf[[
-        'event_date', 'type_of_violence', 'best', 'deaths_civilians', 
+        'event_date', 'violence_type', 'fatalities', 'civilian_deaths', 
         'state', 'lga', 'latitude', 'longitude', 'geometry'
     ]].copy()
 
-    final_gdf.columns = [
-        'event_date', 'event_type', 'fatalities', 'civilian_fatalities',
-        'state', 'lga', 'latitude', 'longitude', 'geometry'
-    ]
-
     final_gdf.to_file(db_path, layer='incidents', driver='SQLite', spatialite=True)
     print(f"Successfully ingested {len(final_gdf)} incidents into the 'incidents' layer.")
+
 
 if __name__ == "__main__":
     ingest_incidents()
